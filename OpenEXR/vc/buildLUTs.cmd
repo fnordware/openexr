c3570151cff9ffee78629dcdc3c2933644628faa
Brendan Bolles <brendan@fnordware.com>
2014-07-21 15:37:34 -0700

    Update vcproj for new stuff
    Includes an updated pre-build script to go through the arduous task of
    building the DWA Lookups header.
    
    b44ExpLogTable also fixed, as the header has been deleted from the
    repository.

--- OpenEXR/vc/buildLUTs.cmd
+++ OpenEXR/vc/buildLUTs.cmd
@@ -1,14 +1,16 @@
-@echo off
+if exist ..\..\..\..\IlmImf\b44ExpLogTable.h goto dwaLut
 
-if exist ..\..\..\..\IlmImf\b44ExpLogTable.h goto skip
-
-set intdir=%1%
-if %intdir%=="" set intdir=Release
-
-cl /nologo /DOPENEXR_DLL /GR /GX /I..\..\..\..\..\..\Deploy\include ..\..\..\..\IlmImf\b44ExpLogTable.cpp /MD /Feb44ExpLogTable.exe /link "..\..\..\..\..\..\Deploy\lib\Release\Half.lib"
-copy ..\..\..\..\..\..\Deploy\bin\%intdir%\Half.dll .
+@cl /nologo /GR /EHsc /I..\..\..\..\..\IlmBase\Half ..\..\..\..\IlmImf\b44ExpLogTable.cpp /MD /Feb44ExpLogTable.exe /link ..\..\..\..\..\IlmBase\vc\vc9\IlmBase\%1%\Half.lib
 .\b44ExpLogTable.exe > ..\..\..\..\IlmImf\b44ExpLogTable.h
-del Half.dll
 del .\b44ExpLogTable.obj .\b44ExpLogTable.exe
 
-:skip
+:dwaLut
+
+if exist ..\..\..\..\IlmImf\dwaLookups.h goto theEnd
+
+@cl /nologo /GR /EHsc /I..\..\..\..\IlmImf /I..\..\..\..\config.windows /I..\..\..\..\..\IlmBase\Half /I..\..\..\..\..\IlmBase\Imath /I..\..\..\..\..\IlmBase\Iex /I..\..\..\..\..\IlmBase\config.windows ..\..\..\..\IlmImf\dwaLookups.cpp /MD /FedwaLookups.exe /link ..\..\..\..\..\IlmBase\vc\vc9\IlmBase\%1%\Half.lib
+echo "Building DWA LUTs...this could take a while..."
+.\dwaLookups.exe > ..\..\..\..\IlmImf\dwaLookups.h
+del .\dwaLookups.obj .\dwaLookups.exe
+
+:theEnd
