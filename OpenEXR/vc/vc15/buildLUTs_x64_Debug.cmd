829fa42616997b18494097105e067ffb8a429f7e
Brendan Bolles <brendan@fnordware.com>
2018-05-13 22:01:35 -0700

    vc15 projects

---
+++ OpenEXR/vc/vc15/buildLUTs_x64_Debug.cmd
@@ -0,0 +1,16 @@
+if exist ..\..\..\..\IlmImf\b44ExpLogTable.h goto dwaLut
+
+@cl /nologo /GR /EHsc /I..\..\..\..\..\IlmBase\Half ..\..\..\..\IlmImf\b44ExpLogTable.cpp /MDd /Feb44ExpLogTable.exe /link ..\..\..\..\..\IlmBase\vc\vc15\IlmBase\x64\Debug\Half.lib
+.\b44ExpLogTable.exe > ..\..\..\..\IlmImf\b44ExpLogTable.h
+del .\b44ExpLogTable.obj .\b44ExpLogTable.exe
+
+:dwaLut
+
+if exist ..\..\..\..\IlmImf\dwaLookups.h goto theEnd
+
+@cl /nologo /GR /EHsc /I..\..\..\..\IlmImf /I..\..\..\..\config.windows /I..\..\..\..\..\IlmBase\Half /I..\..\..\..\..\IlmBase\Imath /I..\..\..\..\..\IlmBase\Iex /I..\..\..\..\..\IlmBase\IlmThread /I..\..\..\..\..\IlmBase\config.windows ..\..\..\..\IlmImf\dwaLookups.cpp /MDd /FedwaLookups.exe /link ..\..\..\..\..\IlmBase\vc\vc15\IlmBase\x64\Debug\Half.lib /link ..\..\..\..\..\IlmBase\vc\vc15\IlmBase\x64\Debug\IlmThread.lib /link ..\..\..\..\..\IlmBase\vc\vc15\IlmBase\x64\Debug\Iex.lib
+echo "Building DWA LUTs...this could take a while..."
+.\dwaLookups.exe > ..\..\..\..\IlmImf\dwaLookups.h
+del .\dwaLookups.obj .\dwaLookups.exe
+
+:theEnd
---
+++ OpenEXR/vc/vc15/buildLUTs_x64_Release.cmd
@@ -0,0 +1,16 @@
+if exist ..\..\..\..\IlmImf\b44ExpLogTable.h goto dwaLut
+
+@cl /nologo /GR /EHsc /I..\..\..\..\..\IlmBase\Half ..\..\..\..\IlmImf\b44ExpLogTable.cpp /MD /Feb44ExpLogTable.exe /link ..\..\..\..\..\IlmBase\vc\vc15\IlmBase\x64\Release\Half.lib
+.\b44ExpLogTable.exe > ..\..\..\..\IlmImf\b44ExpLogTable.h
+del .\b44ExpLogTable.obj .\b44ExpLogTable.exe
+
+:dwaLut
+
+if exist ..\..\..\..\IlmImf\dwaLookups.h goto theEnd
+
+@cl /nologo /GR /EHsc /I..\..\..\..\IlmImf /I..\..\..\..\config.windows /I..\..\..\..\..\IlmBase\Half /I..\..\..\..\..\IlmBase\Imath /I..\..\..\..\..\IlmBase\Iex /I..\..\..\..\..\IlmBase\IlmThread /I..\..\..\..\..\IlmBase\config.windows ..\..\..\..\IlmImf\dwaLookups.cpp /MD /FedwaLookups.exe /link ..\..\..\..\..\IlmBase\vc\vc15\IlmBase\x64\Release\Half.lib /link ..\..\..\..\..\IlmBase\vc\vc15\IlmBase\x64\Release\IlmThread.lib /link ..\..\..\..\..\IlmBase\vc\vc15\IlmBase\x64\Release\Iex.lib
+echo "Building DWA LUTs...this could take a while..."
+.\dwaLookups.exe > ..\..\..\..\IlmImf\dwaLookups.h
+del .\dwaLookups.obj .\dwaLookups.exe
+
+:theEnd
